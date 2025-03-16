30611c89faedea965c3d4c8c283d286a921ecef7
LessaDev <iagolessaferreira@gmail.com>
2025-03-16 10:19:57 -0300

    Create update textTask

--- components/tasks/Task.tsx
+++ components/tasks/Task.tsx
@@ -2,16 +2,28 @@ import { MaterialIcons } from '@expo/vector-icons';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { Text, TouchableOpacity, View } from 'react-native';
 
+import { ModalTextTaskUpdate } from '../modal/ModalTextTaskUpdate';
+
 export type TaskProps = {
+  id: number;
   text: string;
   checked: boolean;
+  modalOpen: boolean;
   handleRemove?: () => void;
   handleChangeChecked?: () => void;
+  handleModalOpen?: () => void;
+  handleUpdateTextTask?: (text: string) => void;
 };
 
-export function Task({ text, handleRemove, checked, handleChangeChecked }: TaskProps) {
+export function Task({
+  text,
+  handleRemove,
+  checked,
+  handleChangeChecked,
+  handleUpdateTextTask,
+}: TaskProps) {
   return (
-    <View className="max-w-screen mb-4 flex min-h-28 w-full flex-row items-center justify-center gap-8 rounded-md bg-gray-600 px-8 ">
+    <View className="max-w-screen mb-4 flex min-h-28 w-full flex-row items-center justify-between gap-4 rounded-md bg-gray-600 px-4 ">
       <TouchableOpacity
         className=" flex h-12 w-12 items-center justify-center "
         onPress={handleChangeChecked}>
@@ -24,23 +36,27 @@ export function Task({ text, handleRemove, checked, handleChangeChecked }: TaskP
         </View>
       </TouchableOpacity>
 
-      <View className="max-w-sm flex-1">
-        <View>
-          {checked ? (
-            <Text className=" text-xl text-gray-300 line-through">{text} </Text>
-          ) : (
-            <Text className=" text-xl text-white ">{text} </Text>
-          )}
-        </View>
+      <View>
+        {checked ? (
+          <Text className=" text-xl text-gray-300 line-through">{text} </Text>
+        ) : (
+          <Text className=" text-xl text-white ">{text} </Text>
+        )}
       </View>
-      <TouchableOpacity
-        className=" flex h-12 w-12 items-center justify-center"
-        onPress={handleRemove}>
-        <Ionicons name="trash-outline" size={30} color="white" />
-      </TouchableOpacity>
+
+      <View className="flex flex-row ">
+        <TouchableOpacity
+          className=" flex h-12 w-12 items-center justify-center"
+          onPress={handleRemove}>
+          <Ionicons name="trash-outline" size={30} color="white" />
+        </TouchableOpacity>
+        <ModalTextTaskUpdate
+          text={text}
+          handleUpdateText={(text: string) =>
+            handleUpdateTextTask ? handleUpdateTextTask(text) : console.log(text)
+          }
+        />
+      </View>
     </View>
   );
-}
-function setTask(arg0: (prevState: any) => any[]): any {
-  throw new Error('Function not implemented.');
 }
